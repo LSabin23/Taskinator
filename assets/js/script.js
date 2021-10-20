4b235012211f0dab947db5afec06f12f20751a61
unknown <44147809+LSabin23@users.noreply.github.com>
2021-10-20 19:10:32 -0400

    added task actions to list items

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+var taskIdCounter = 0
 var formEl = document.querySelector('#task-form')
 var tasksToDoEl = document.querySelector('#tasks-to-do')
 
@@ -29,18 +30,71 @@ var createTaskEl = function (taskDataObj) {
   var listItemEl = document.createElement('li')
   listItemEl.className = 'task-item'
 
+  // add task id as custom attribute
+  listItemEl.setAttribute('data-task-id', taskIdCounter)
+
   // create div to hold task info and add to list item
   var taskInfoEl = document.createElement('div')
-
-  // give it a class name
   taskInfoEl.className = 'task-info'
 
-  // add HTML content to div
   taskInfoEl.innerHTML = '<h3 class=\'task-name\'>' + taskDataObj.name + '</h3><span class=\'task-type\'>' + taskDataObj.type + '</span>'
   listItemEl.appendChild(taskInfoEl)
 
+  // assign the completed actionContainerEl with the associated task id to an element we can add to our task list
+  var taskActionsEl = createTaskActions(taskIdCounter)
+  listItemEl.appendChild(taskActionsEl)
+
   // add entire list item to list
   tasksToDoEl.appendChild(listItemEl)
+
+  // increase task counter for next unique id
+  taskIdCounter++
+}
+
+// parameter of taskId is how we pass a different id to the fxn to keep track of which elements we're creating for which task
+var createTaskActions = function (taskId) {
+  // create a new div that will contain the new task action elements
+  var actionContainerEl = document.createElement('div')
+  actionContainerEl.className = 'task-actions'
+
+  var editButtonEl = document.createElement('button')
+  editButtonEl.textContent = 'Edit'
+  editButtonEl.className = 'btn edit-btn'
+  editButtonEl.setAttribute('data-task-id', taskId)
+
+  actionContainerEl.appendChild(editButtonEl)
+
+  var deleteButtonEl = document.createElement('button')
+  deleteButtonEl.textContent = 'Delete'
+  deleteButtonEl.className = 'btn delete-btn'
+  deleteButtonEl.setAttribute('data-task-id', taskId)
+
+  actionContainerEl.appendChild(deleteButtonEl)
+
+  var statusSelectEl = document.createElement('select')
+  statusSelectEl.className = 'select-status'
+  statusSelectEl.setAttribute('name', 'status-change')
+  statusSelectEl.setAttribute('data-task-id', taskId)
+
+  var statusChoices = ['To Do', 'In Progress', 'Completed']
+
+  for (var i = 0; i < statusChoices.length; i++) {
+    // create new option element
+    var statusOptionEl = document.createElement('option')
+
+    // add text content to option element
+    statusOptionEl.textContent = statusChoices[i]
+
+    // set value attribute for option element to be the same as the text content (the array item name)
+    statusOptionEl.setAttribute('value', statusChoices[i])
+
+    // append option element to select element
+    statusSelectEl.appendChild(statusOptionEl)
+  }
+
+  actionContainerEl.appendChild(statusSelectEl)
+
+  return actionContainerEl
 }
 
 formEl.addEventListener('submit', taskFormHandler)
