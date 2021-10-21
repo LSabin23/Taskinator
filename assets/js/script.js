2a18b4f108cfee38ace11a31d30ebc0810ad9743
unknown <44147809+LSabin23@users.noreply.github.com>
2021-10-20 22:53:59 -0400

    added functionality to change task column based on dropdown choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ var pageContentEl = document.querySelector('#page-content')
 var taskIdCounter = 0
 var formEl = document.querySelector('#task-form')
 var tasksToDoEl = document.querySelector('#tasks-to-do')
+var tasksInProgressEl = document.querySelector('#tasks-in-progress')
+var tasksCompletedEl = document.querySelector('#tasks-completed')
 
 var taskFormHandler = function (event) {
   event.preventDefault()
@@ -17,14 +19,35 @@ var taskFormHandler = function (event) {
 
   formEl.reset()
 
-  // package up data as an object
-  var taskDataObj = {
-    name: taskNameInput,
-    type: taskTypeInput
-  }
+  var isEdit = formEl.hasAttribute('data-task-id')
 
-  // send it as an argument to createTaskEl
-  createTaskEl(taskDataObj)
+  // has data attribute, so get task id and call fxn to complete edit process
+  if (isEdit) {
+    var taskId = formEl.getAttribute('data-task-id')
+    completeEditTask(taskNameInput, taskTypeInput, taskId)
+  }
+  // no data attribute, so create object as normal and pass to createTaskEl fxn
+  else {
+    var taskDataObj = {
+      name: taskNameInput,
+      type: taskTypeInput
+    }
+    // send it as an argument to createTaskEl
+    createTaskEl(taskDataObj)
+  }
+}
+
+var completeEditTask = function (taskName, taskType, taskId) {
+  var taskSelected = document.querySelector('.task-item[data-task-id=\'' + taskId + '\']')
+
+  taskSelected.querySelector('h3.task-name').textContent = taskName
+  taskSelected.querySelector('span.task-type').textContent = taskType
+
+  alert('Task Updated!')
+
+  // reset the form by removing the task id and changing the button text back to normal
+  formEl.removeAttribute('data-task-id')
+  document.querySelector('#save-task').textContent = 'Add Task'
 }
 
 var createTaskEl = function (taskDataObj) {
@@ -140,3 +163,25 @@ var editTask = function (taskId) {
   // add the task's id to save the changes to correct task
   formEl.setAttribute('data-task-id', taskId)
 }
+
+var taskStatusChangeHandler = function (event) {
+  var taskId = event.target.getAttribute('data-task-id')
+
+  // get the currently selected option's value and convert to lowercase
+  var statusValue = event.target.value.toLowerCase()
+
+  // find the parent task item element based on the id
+  var taskSelected = document.querySelector('.task-item[data-task-id=\'' + taskId + '\']')
+
+  if (statusValue === 'to do') {
+    tasksToDoEl.appendChild(taskSelected)
+  }
+  else if (statusValue === 'in progress') {
+    tasksInProgressEl.appendChild(taskSelected)
+  }
+  else if (statusValue === 'completed') {
+    tasksCompletedEl.appendChild(taskSelected)
+  }
+}
+
+pageContentEl.addEventListener('change', taskStatusChangeHandler)
